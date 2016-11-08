55ed439fa2100d68d4c37e9f4c153a9f84766993
Briana Eng <brianaeng@gmail.com>
2016-11-08 15:22:47 -0800

    Trying to add errors

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -9,34 +9,37 @@ class EstimatesController < ApplicationController
   #Users requests should be logged so that responses could be audited
 
 
-  #Add order ID
+  #Add order ID?
 
   def shipping_rate
+    # if params[:weight] == nil || params[:zip] == nil
+    #   render :json => [], :status => :bad_request
+    # end
     #This will get all estimates from shipping providers
     package = ActiveShipping::Package.new((params[:weight].to_i * 16), [15, 10, 4.5])
 
-    origin = ActiveShipping::Location.new(country: 'US', state: 'WA', city: 'Seattle', zip: '98161')
+    origin = ActiveShipping::Location.new(country: 'US', zip: '98161')
 
-    #I might only need zip code?
-    destination = ActiveShipping::Location.new(country: 'US', state: params[:state].to_s, city: params[:city].to_s, postal_code: params[:zip].to_s)
+    #Switching to only zip (can later add state and city if needed)
+    destination = ActiveShipping::Location.new(country: 'US', postal_code: params[:zip].to_s)
 
     #UPS ESTIMATE
     if params[:service_id].upcase == "UPS"
       ups = ActiveShipping::UPS.new(login: ENV["ACTIVESHIPPING_UPS_LOGIN"], password: ENV["ACTIVESHIPPING_UPS_PASSWORD"], key: ENV["ACTIVESHIPPING_UPS_KEY"])
       response = ups.find_rates(origin, destination, package)
-      # ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
-      # estimate = Estimate.create(name: "UPS", costs: ups_rates)
+      ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+      estimate = Estimate.create(name: "UPS", costs: ups_rates)
     #USPS ESTIMATE
     elsif params[:service_id].upcase == "USPS"
       usps = ActiveShipping::USPS.new(login: ENV["ACTIVESHIPPING_USPS_LOGIN"])
       response = usps.find_rates(origin, destination, package)
-      # usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
-      # estimate = Estimate.create(name: "USPS", costs: usps_rates)
+      usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+      estimate = Estimate.create(name: "USPS", costs: usps_rates)
     end
 
     #Passes the estimates as json
-    if response != nil
-      render :json => response, :status => :ok
+    if estimate != nil
+      render :json => estimate, :status => :ok
     else
       render :json => [], :status => :no_content
     end
